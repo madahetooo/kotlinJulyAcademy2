ff3c3ac0adbdb6e7d2bdea4c2fbd259dd637566b
Eslam Medhat <28203059+madahetooo@users.noreply.github.com>
2023-07-02 20:51:22 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,99 @@
+fun main() {
+    // Arithmetic Operator
+//    var x = 4
+//    var y = 4
+//    println(x + y)
+//    println(x - y)
+//    println(x * y)
+//    println(x / y)
+
+
+    // Relational Operator
+//    var x =4
+//    var y =4
+//    if (x > y){
+//        println("X is larger than Y")
+//    }
+//    else if (x < y){
+//        println("X is smaller than Y")
+//    }
+//    else{
+//        println("X is equal to Y")
+//
+//    }
+
+
+    //Assignment Operator
+//
+//    var x = 5
+////    x += 1
+//    x -= 1
+//    println(x)
+
+
+    //Unary Operator
+    // ++ --
+//    var x = 5
+//    println(x++) // 5
+//    println(x) // 6
+//    println(++x) // 7
+//    println(x) //7
+//    println(x--) //7
+//    println(x) //6
+//    println(--x) //5
+//    println(x)//5
+
+
+    //Logical Operator
+//    var x = 8 // Initialize
+//    var y = 7
+//    var z = 10
+//
+//    if (x > y  ||  x > z){
+//        println("x is  large number")
+//    }
+//    if (x != 7 ) { //true
+//        println("x is not equal to 7")
+//    }
+//    if (x === 8){
+//        println("X is equal to 8")
+//    }
+
+
+    //Read data from user
+    //var name="Eslam" // Initialize
+
+//    var name:String // Declaration
+//    var age:Int
+//    println("Please enter your name ?")
+//    name = readLine().toString()
+//    println("Welcome $name")
+//    println("And your age please")
+//    age = Integer.valueOf(readLine().toString())
+//    println("Ok $name your age is $age")
+
+
+//    var season = 10
+//    when(season){
+//        12,1,2,3 -> print("Winter Season")
+//        4,5,6,7 -> print("Autumn Season")
+//        8,9,10,11 -> print("Summer Season")
+//    }
+
+
+
+
+//    var degree: Int
+//
+//    println("Welcome to our college website")
+//    println("Please enter your degree to get your result")
+//    degree = readln().toInt()
+//    when (degree) {
+//        in 0..49 -> print("You Failed")
+//        in 50..64 -> print("You Got D")
+//        in 65..74 -> print("You C")
+//        in 75..84 -> print("You B")
+//        in 85..100 -> print("You A+")
+//        else -> print("Not Valid Degree")
+//    }
+}
